ca5e95b63894c9491b5bcb57314fe431948373b6
Moonlock <moonlock@riseup.net>
2016-02-10 11:57:58 -0600

    Clients xor messages with keys before sending.

--- client.c
+++ client.c
@@ -10,18 +10,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <semaphore.h>
 
 #include <openssl/dh.h>
 #include <openssl/bn.h>
+#include <openssl/hmac.h>
 #include <openssl/rand.h>
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-#define PORT "30001" // the port client will be connecting to 
+#define PORT "30001" /* The port the server is listening on. */
 #define BACKLOG 10
-#define MAXDATASIZE 500 // max number of bytes we can get at once 
+#define MAXDATASIZE 500 /* Max number of bytes client can receive at once. */
+#define MAXMESSAGESIZE 160
 
-DH *dh;		//Diffie-Hellman key
+
+DH* dh;								/* Diffie-Hellman key. */
+unsigned char *SKRight, *SKLeft;	/* Keys user shares with clients on 
+									   their 'left' and 'right'. */
 
 typedef struct client
 {
@@ -29,7 +33,8 @@ typedef struct client
     char port[6];
 } CLIENT;
 
-// get sockaddr, IPv4 or IPv6:
+
+/* Get sockaddr, IPv4 or IPv6. */
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -39,48 +44,39 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void *StartDH(void* client)
+void* StartDH(void* client)
 {
-	//Send 
-
 	CLIENT* c = (CLIENT*)client;
-	int client_fd;
-	int rv;
-	int numbytes;
+	BIGNUM* pubKey = NULL;
+
+	int fd;
 	struct addrinfo hints, *servinfo, *p;
 	char remoteIP[INET6_ADDRSTRLEN];
 	char buf[MAXDATASIZE];
+	int rv, numbytes;
 
-    BIGNUM *pubKey = NULL;
-    unsigned char *key = NULL;
-
-	//DH *dh;
-	//int prime_len = 1024;
-	//int generator = 2;
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	
-	printf(" - %s - \n", c->hostname);
 
 	if ((rv = getaddrinfo(c->hostname, c->port, &hints, &servinfo)) != 0)
 	{
-		fprintf(stderr, "getaddrinfo(connect): %s\n", gai_strerror(rv));
-		exit(1);
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		exit(-1);
 	}
 
 	for(p = servinfo; p != NULL; p = p->ai_next)
 	{
-		if ((client_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+		if ((fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
 		{
 			perror("client: socket");
 			continue;
 		}
 
-		if (connect(client_fd, p->ai_addr, p->ai_addrlen) == -1)
+		if (connect(fd, p->ai_addr, p->ai_addrlen) == -1)
 		{
-			close(client_fd);
+			close(fd);
 			perror("client: connect");
 			continue;
 		}
@@ -90,210 +86,212 @@ void *StartDH(void* client)
 
 	if (p == NULL)
 	{
-		fprintf(stderr, "client: failed to connect\n");
-		exit(1);
+		fprintf(stderr, "client: failed to connect.\n");
+		exit(-1);
 	}
 
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-		remoteIP, sizeof remoteIP);
-	printf("client: connecting to %s\n", remoteIP);
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), remoteIP, sizeof remoteIP);
+	printf("[DEBUG] Connecting to %s.\n", remoteIP);
 
 	freeaddrinfo(servinfo);
 
-	// Diffie-Hellman
-	//dh = DH_new();
-	//DH_generate_parameters_ex(dh, prime_len, generator, NULL);
-
-	//DH_free(dh);
-
-/*	if (DH_generate_key(dh) == 0)
-	{
-		perror("DH_generate_key");
-		exit(1);
-	}
-*/	printf("  ->%s\n", BN_bn2hex(dh->pub_key));
-
-	if ((numbytes = send(client_fd, BN_bn2hex(dh->pub_key), strlen(BN_bn2hex(dh->pub_key)), 0)) == -1)
+	if ((numbytes = send(fd, BN_bn2hex(dh->pub_key), strlen(BN_bn2hex(dh->pub_key)), 0)) == -1)
 	{
 		perror("send");
-		exit(1);
+		exit(-1);
 	}
 
-	if ((numbytes = recv(client_fd, buf, MAXDATASIZE-1, 0)) == -1) 
-    {
-        perror("recv(client)");
-        exit(1);
-    }
+	if ((numbytes = recv(fd, buf, MAXDATASIZE-1, 0)) == -1)
+	{
+		perror("client: recv");
+		exit(-1);
+	}
 
-    buf[numbytes] = '\0';
-    BN_hex2bn(&pubKey, buf);
-    //printf(" dh: %s\n", buf);
-    printf(" dh(send): %s\n", BN_bn2hex(pubKey));
+	buf[numbytes] = '\0';
+	BN_hex2bn(&pubKey, buf);
 
-    key = OPENSSL_malloc(sizeof(unsigned char) * (DH_size(dh)));
-    if(DH_compute_key(key, pubKey, dh) == -1)
-    {
-    	perror("DH_compute_key");
-    	exit(1);
-    }
-    //printf(" key(start): %s\n", key);
+	SKRight = OPENSSL_malloc(sizeof(unsigned char) * (DH_size(dh)));
+	if(DH_compute_key((unsigned char *)SKRight, pubKey, dh) == -1)
+	{
+		perror("DH_compute_key");
+		exit(-1);
+	}
 
-	return NULL;//client_fd;
+	// TODO: Key extraction.
+
+	close(fd);
+
+	return NULL;
 }
 
-int ConnectToClient(char* sport, char *hostname, char* rport)
+int ConnectToClient(char* local_port, char* hostname, char* remote_port)
 {
-	//Receive
-
-	pthread_t startThread;
-	void *status;
 	CLIENT c;
+	BIGNUM* pubKey = NULL;
 
-	int rv;
-	int yes = 1;
-	int listener, new_fd;
-	int numbytes;
-	char buf[MAXDATASIZE];
+	pthread_t DH_thread;
+	void* status;
+
+	int listener, fd;
 	struct addrinfo hints, *servinfo, *p;
 	struct sockaddr_storage their_addr;
-    socklen_t sin_size;
+	socklen_t sin_size;
+	int yes = 1;
 
-    BIGNUM *pubKey = NULL;
-    unsigned char *key = NULL;
+	int rv, numbytes;
+	char buf[MAXDATASIZE];
 
- 	memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; 
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET; 
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-   	if (DH_generate_key(dh) == 0)
-	{
-		perror("DH_generate_key");
-		exit(1);
-	}
-
-    printf("	%s, %s, %s", sport, hostname, rport);
-
-    if ((rv = getaddrinfo(NULL, sport, &hints, &servinfo)) != 0) 
+    if (DH_generate_key(dh) == 0)
     {
-    	fprintf(stderr, "getaddrinfo(receive): %s\n", gai_strerror(rv));
-    	return 1;
+    	perror("client: DH_generate_key");
+    	exit(-1);
+    }
+
+    printf("[DEBUG] lp:%s, h:%s, rp:%s\n", local_port, hostname, remote_port);
+
+    if ((rv = getaddrinfo(NULL, local_port, &hints, &servinfo)) != 0)
+    {
+    	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    	exit(-1);
     }
 
     /* Loop through all the results and bind to the first we can. */
-    for(p = servinfo; p != NULL; p = p->ai_next) 
+    for (p = servinfo; p != NULL; p = p->ai_next)
     {
-    	if ((listener = socket(p->ai_family, p->ai_socktype,
-    		p->ai_protocol)) == -1) 
+    	if ((listener = socket(p->ai_family, p->ai_socktype, 
+    		p->ai_protocol)) == -1)
     	{
-    		perror("server: socket");
+    		perror("client: socket");
     		continue;
     	}
 
-    	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) 
-        {
-            perror("setsockopt");
-            return 1;
-        }
+    	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    	{
+    		perror("client: setsockopt");
+    		exit(-1);
+    	}
 
-    	if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) 
+    	if (bind(listener, p->ai_addr, p->ai_addrlen) == -1)
     	{
     		close(listener);
-    		perror("server: bind");
+    		perror("client: bind");
     		continue;
     	}
 
     	break;
     }
 
-    if (p == NULL) 
+    if (p == NULL)
     {
-    	fprintf(stderr, "server: failed to bind socket\n");
-    	return 2;
+    	fprintf(stderr, "client: failed to bind socket.\n");
+    	exit(-1);
     }
 
     freeaddrinfo(servinfo);
 
-    if (listen(listener, BACKLOG) == -1) 
+    if (listen(listener, BACKLOG) == -1)
     {
-        perror("listen");
-        return 1;
+    	perror("client: listen");
+    	exit(-1);
     }
 
     strcpy(c.hostname, hostname);
-    strcpy(c.port, rport);
-    rv = pthread_create(&startThread, NULL, StartDH, &c);
+    strcpy(c.port, remote_port);
+    usleep(1000000);
+    rv = pthread_create(&DH_thread, NULL, StartDH, &c);
 
     sin_size = sizeof their_addr;
-    new_fd = accept(listener, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) 
+    fd = accept(listener, (struct sockaddr *)&their_addr, &sin_size);
+    if (fd == -1)
     {
-        perror("accept");
-        return 1;
+    	perror("client: accept");
+    	exit(-1);
     }
 
-    if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) 
+    if ((numbytes = recv(fd, buf, MAXDATASIZE-1, 0)) == -1)
     {
-        perror("recv(client)");
-        return 1;
+    	perror("client: recv");
+    	exit(-1);
     }
 
-	printf("  -->%s\n", BN_bn2hex(dh->pub_key));
+    if (send(fd, BN_bn2hex(dh->pub_key), strlen(BN_bn2hex(dh->pub_key)), 0) == -1)
+    {
+    	perror("client: send");
+    	exit(-1);
+    }
 
-    if (send(new_fd, BN_bn2hex(dh->pub_key), strlen(BN_bn2hex(dh->pub_key)), 0) == -1)
-	{
-		perror("send");
-		exit(1);
-	}
-
-    rv = pthread_join(startThread, &status);
+    rv = pthread_join(DH_thread, &status);
+    if (rv != 0)
+    {
+    	perror("client: pthread_join");
+    	exit(-1);
+    }
 
     buf[numbytes] = '\0';
-    BN_hex2bn(&pubKey, buf);
-    //printf(" dh: %s\n", buf);
-    printf(" dh(return): %s\n", BN_bn2hex(pubKey));
+    BN_hex2bn(&pubKey, buf);	// TODO: Error checking.
 
-    key = OPENSSL_malloc(sizeof(unsigned char) * (DH_size(dh)));
-    if(DH_compute_key(key, pubKey, dh) == -1)
+    SKLeft = OPENSSL_malloc(sizeof(unsigned char) * (DH_size(dh)));	
+    							// Here too.
+    
+    if (DH_compute_key((unsigned char *)SKLeft, pubKey, dh) == -1)
     {
     	perror("DH_compute_key");
-    	exit(1);
+    	exit(-1);
     }
-    //printf(" key(connect): %s\n", key);
+
+    // TODO: Key extraction.
 
     close(listener);
-    //rv = pthread_join(startThread, &status);
+    close(fd);
 
-	return 0;
+    return 0;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char const *argv[])
 {
 	int server_fd, client_fd;
-	int numbytes;
-	char buf[MAXDATASIZE];
 	struct addrinfo hints, *servinfo, *p;
-	int rv;
 	char remoteIP[INET6_ADDRSTRLEN];
-
-	char msg[MAXDATASIZE];
 	char *serv_hostname, *client_hostname;
-	char *sport, *rport;
-	int command, ready;
+	char *local_port, *remote_port;
+	int rv, numbytes;
+	int i, j, command, ready;
 
-    struct timeval tv;
-    fd_set readfds;
+	char buf[MAXDATASIZE];
+	char msgRecv[MAXDATASIZE], msgSend[MAXDATASIZE];
+
+	struct timeval tv;
+	fd_set readfs;
+
+	char dataR[MAXDATASIZE], dataL[MAXDATASIZE];
+	char xorKeyR[257], xorKeyL[257];
+	unsigned char *hashR = malloc(sizeof(unsigned char) * 65);	/* 512bit hash, plus null byte. */
+	unsigned char *hashL = malloc(sizeof(unsigned char) * 65);
+	int nHKDF = 0;
 
 
+	strcpy(dataR, "");
+	strcpy(dataL, "");
+	
 	if (argc != 2)
 	{
-		fprintf(stderr, "usage: ./client hostname");
-		exit(1);
+		printf("usage: ./client [hostname].\n");
+		exit(-1);
 	}
 	serv_hostname = argv[1];
 
 	dh = DH_new();
+	if (dh == NULL)
+	{
+		perror("client: DH_new");
+		exit(-1);
+	}
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_INET;
@@ -302,7 +300,7 @@ int main(int argc, char* argv[])
 	if ((rv = getaddrinfo(serv_hostname, PORT, &hints, &servinfo)) != 0)
 	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+		exit(-1);
 	}
 
 	for(p = servinfo; p != NULL; p = p->ai_next)
@@ -326,66 +324,72 @@ int main(int argc, char* argv[])
 	if (p == NULL)
 	{
 		fprintf(stderr, "client: failed to connect\n");
-		return 2;
+		exit(-1);
 	}
 
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-		remoteIP, sizeof remoteIP);
-	printf("client: connecting to %s\n", remoteIP);
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), remoteIP, sizeof remoteIP);
+	printf("Connecting to %s\n", remoteIP);
 
 	freeaddrinfo(servinfo);
 
-	// Receive Diffie-Hellman prime
+	/* Receive Diffie-Hellman prime. */
 	if ((numbytes = recv(server_fd, buf, MAXDATASIZE-1, 0)) == -1)
 	{
 		perror("recv");
-		exit(1);
+		exit(-1);
 	}
 	buf[numbytes] = '\0';
 
-	printf("   %s\n", buf);
-	strcpy(msg, strtok(buf, ":"));
-	BN_hex2bn(&dh->p, msg);
+	BN_hex2bn(&dh->p, strtok(buf, ":"));
 	BN_dec2bn(&dh->g, "2");
-	strcpy(buf, strtok(NULL, "\0"));
-	//printf("  p = %s\n", BN_bn2hex(dh->p));
-// int BN_dec2bn(BIGNUM **a, const char *str);
-	
 
+	command = atoi(strtok(NULL, ":"));
 
 	while(1)
 	{
-		// Receive - 0:(wait)
-		//			 1:[sender port]:[hostname]:[receiver port]
-		//			 2:[message]
-	/*	if ((numbytes = recv(server_fd, buf, MAXDATASIZE-1, 0)) == -1)
-		{
-			perror("recv");
-			exit(1);
-		}
-		buf[numbytes] = '\0';
-	*/
-		printf("%s\n", buf);
-		command = atoi(strtok(buf, ":"));
-		printf("%i\n", command);
+		/* Receive - 0:(wait)
+					 1:[local port]:[hostname]:[remote port]
+					 2:[message]
+					 3:[message]
+		*/
 
 		switch (command)
 		{
-		case 0:	// Wait
+		case 0:	/* Wait. */
 			ready = 0;
 			printf("Waiting for Bruce Schneier...\n");
 			break;
-		case 1:	// Get hostname, ports
+		case 1:	/* Get hostname and ports. */
 			ready = 1;
-			sport = strtok(NULL, ":");
+			local_port = strtok(NULL, ":");
 			client_hostname = strtok(NULL, ":");
-			rport = strtok(NULL, "\0");
-			client_fd = ConnectToClient(sport, client_hostname, rport);
+			remote_port = strtok(NULL, "\0");
+			client_fd = ConnectToClient(local_port, client_hostname, remote_port);
+			
+			printf(">>");
+			fflush(stdout);
 			break;
-		case 2:	// Receive message
+		case 2:	/* Receive message. */
 			ready = 1;
-			strcpy(msg, strtok(NULL, "\0"));
-			printf("RECEIVED: %s\n", msg);
+			strcpy(msgRecv, strtok(NULL, "\0"));	// Segfault due to copying NULL.
+
+			if (msgRecv != NULL)
+			{
+				printf("RECEIVED: '%s'\n", msgRecv);
+				printf(">>");
+				fflush(stdout);
+			}
+			break;
+		case 3: /* Receive message, then wait for new connections info. */
+			ready = 0;
+			strcpy(msgRecv, strtok(NULL, "\0"));
+
+			if (msgRecv != NULL)
+			{
+				printf("RECEIVED: '%s'\n", msgRecv);
+				printf(">>");
+				fflush(stdout);
+			}
 			break;
 		default:
 			fprintf(stderr, "Something weird happened...\n");
@@ -394,76 +398,74 @@ int main(int argc, char* argv[])
 
 		if (ready == 1)
 		{
-		//Generate random data
-		//Send
-		//Receive
-		//Select (replace scanf)
-		//XOR
-			strcpy(msg, " ");
-			//printf("%s\n", msg);
-			printf(">>");
-			fflush(stdout);
-			//scanf("%s", msg);
+			strcpy(msgSend, "");
+
 			tv.tv_sec = 5;
 			tv.tv_usec = 0;
 
-			FD_ZERO(&readfds);
-			FD_SET(0, &readfds);
+			FD_ZERO(&readfs);
+			FD_SET(0, &readfs);
 
-			if (select(1, &readfds, NULL, NULL, &tv) == -1)
+			if (select(1, &readfs, NULL, NULL, &tv) == -1)
 			{
-            	perror("select");
-            	exit(4);
-        	}
-
-			//read
-			//fgets(msg, sizeof(msg), stdin);
-
-			if (FD_ISSET(0, &readfds))
-			{
-				fgets(msg, sizeof(msg), stdin);
-				printf("  sent \"%s\"\n", msg);
+				perror("select");
+				exit(-1);
 			}
-			else
-			{
-				printf("  timeout \"%s\" (%lu)\n", msg, strlen(msg));
-			}
-		//printf("Sending %s\n", msg);
 
-			if (send(server_fd, msg, strlen(msg), 0) == -1)
+			/* Read message from stdin. */
+			if (FD_ISSET(0, &readfs))
+			{
+				fgets(msgSend, sizeof(msgSend), stdin);
+				printf("[DEBUG] Sent '%s'\n", msgSend);
+			}
+
+			/* Get keys to xor with message. */
+			for (i = 0; i < 4; i++)
+			{
+				nHKDF++;
+				memcpy(hashR, HMAC(EVP_sha512(), SKRight, 128, (unsigned char*)dataR, 
+					strlen(dataR), NULL, NULL), 65);
+				memcpy(hashL, HMAC(EVP_sha512(), SKLeft, 128, (unsigned char*)dataL, 
+					strlen(dataL), NULL, NULL), 65);
+				sprintf(&xorKeyR[i*64], "%s", hashR);
+				sprintf(&xorKeyL[i*64], "%s", hashL);
+
+				sprintf(dataR, "%s%c%c%c%c", hashR, (nHKDF >> 24) & 0xFF, (nHKDF >> 16) & 0xFF,
+					(nHKDF >> 8) & 0xFF, nHKDF & 0xFF);
+				sprintf(dataL, "%s%c%c%c%c", hashL, (nHKDF >> 24) & 0xFF, (nHKDF >> 16) & 0xFF,
+					(nHKDF >> 8) & 0xFF, nHKDF & 0xFF);
+			}
+
+			/* xor message with keys. */
+			for (i = 0; i < strlen(msgSend); i++)
+			{
+				msgSend[i] ^= (xorKeyR[i] ^ xorKeyL[i]);
+			}
+			for (; i < strlen(xorKeyR); i++)
+			{
+				msgSend[i] = xorKeyR[i] ^ xorKeyL[i];
+			}
+
+			/* Send xor'd message to server. */
+			if (send(server_fd, msgSend, strlen(msgSend), 0) == -1)
 			{
 				perror("send");
-				exit(1);
+				exit(-1);
 			}
 		}
 
 		if ((numbytes = recv(server_fd, buf, MAXDATASIZE-1, 0)) == -1)
 		{
 			perror("recv");
-			exit(1);
+			exit(-1);
 		}
 		buf[numbytes] = '\0';
+
+		command = atoi(strtok(buf, ":"));
 	}
+
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
